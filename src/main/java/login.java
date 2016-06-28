d992a03a71824769face924187cc3232cfd9b3c2
Manu <gavrismanuela1993@gmail.com>
2016-06-28 14:38:31 +0300

    working login app with data from the database

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,4 +1,3 @@
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -6,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 import java.util.ArrayList;
 
 
@@ -16,25 +16,84 @@ import java.util.ArrayList;
 
 @WebServlet("/login")
 public class login extends HttpServlet {
-    ArrayList<User> l = new ArrayList<User>();
+    ArrayList<User> list = new ArrayList<User>();
+
+
+    public ArrayList<User> demoRead() throws ClassNotFoundException, SQLException {
+        try {// 1. load driver
+            Class.forName("org.postgresql.Driver");
+
+            // 2. define connection params to db
+            final String URL = "jdbc:postgresql://54.93.65.5/4_Manu";
+            final String USERNAME = "fasttrackit_dev";
+            final String PASSWORD = "fasttrackit_dev";
+
+            // 3. obtain a connection
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            // 4. create a query statement
+            Statement st = conn.createStatement();
+
+            // 5. execute a query
+            ResultSet rs = st.executeQuery("SELECT username,password FROM login");
+
+            // 6. iterate the result set and print the values
+
+
+            while (rs.next()) {
+                User user = new User();
+                String usr = rs.getString("username").trim();
+                String pass = rs.getString("password").trim();
+                user.setU(usr);
+                user.setP(pass);
+                list.add(user);
+                System.out.println(user.getU() + " - " + user.getP());
+            }
+
+
+            // 7. close the objects
+            rs.close();
+            st.close();
+            conn.close();
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
     @Override
     public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+
+
         resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out =resp.getWriter();
+        PrintWriter out = resp.getWriter();
+
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-//        System.out.println("corect: "+username);
-//        System.out.println("corect: "+password);
+        try {
+            demoRead();
+            System.out.println("hello");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        boolean log=false;
-        for (User u : l) {
+        boolean log = false;
+
+        for (User u : list) {
             if (u.getU().equals(username) && u.getP().equals(password)) {
-                log=true;
+                log = true;
                 break;
             }
 
         }
-        if(log==true){
+        if (log == true) {
             System.out.println("success!");
             out.println("<html>");
             out.println("<head>");
@@ -42,16 +101,13 @@ public class login extends HttpServlet {
 
             out.println("<body>");
 
-
-            out.println("<b>"+"Te-ai logat cu succes!"+"</b>");
+            out.println("<b>" + "Te-ai logat cu succes!" + "</b>");
 
             out.close();
 
 
-
             out.println("</body>");
-        }
-        else{
+        } else {
             System.out.println("try again");
             out.println("<html>");
             out.println("<head>");
@@ -60,10 +116,9 @@ public class login extends HttpServlet {
             out.println("<body>");
 
 
-            out.println("<b>"+"Username sau parola gresita!"+"</b>");
+            out.println("<b>" + "Username sau parola gresita!" + "</b>");
 
             out.close();
-
 
 
             out.println("</body>");
@@ -71,16 +126,10 @@ public class login extends HttpServlet {
 
     }
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        User u1=new User();
-        u1.setU("manuela");
-        u1.setP("parola");
-        User u2=new User();
-        u2.setU("admin");
-        u2.setP("12345");
 
-        l.add(u1);
-        l.add(u2);
-    }
 }
+
+
+
+
+
